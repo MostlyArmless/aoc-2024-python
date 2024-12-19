fb8bd54926adcb1840812d510e87cbe562471796
Mike Thiem <mike.j.thiem@gmail.com>
2024-12-19 10:46:02 -0800

    Solved day 9 part 1

--- new_day.py
+++ new_day.py
@@ -1,16 +1,22 @@
 def new_day(day: int):
-    text = """
-from utils import timer
+    text = """from utils import timer
 
 sample_input = '''stuff'''
 real_input = '''things'''
 
 @timer
-def part1(input: str):
-    pass
+def part1(input: str) -> int:
+    return 0
+    
+@timer
+def part2(input: str) -> int:
+    return 0
 
 assert part1(sample_input) == 42
 # assert part1(real_input) == 42
+
+# assert part2(sample_input) == 42
+# assert part2(real_input) == 42
 """
     with open(f'{day}.py', 'w') as stream:
         stream.write(text)
